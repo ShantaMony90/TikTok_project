59f78b391a6a5969735f3b75599ed94eba519094
ShantaMony90 <shantamony90@gmail.com>
2025-07-01 23:46:04 +0600

    first commit

---
+++ a_posts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request):
+  context = {
+    'page' : 'Home',
+    'partial' : request.htmx,
+  }
+  if request.htmx:
+    return render(request, 'a_posts/partials/_home.html', context)
+  return render(request, 'a_posts/home.html', context)
+
+def explore_view(request):
+  context = {
+    'page' : 'Explore',
+    'partial' : request.htmx,
+  }
+  if request.htmx:
+    return render(request, 'a_posts/partials/_explore.html', context)
+  return render(request, 'a_posts/explore.html', context)
+
+def upload_view(request):
+  context = {
+    'page' :'Upload',
+    'partial' : request.htmx,
+  }
+  if request.htmx:
+    return render(request, 'a_posts/partials/_upload.html', context)
+  return render(request, 'a_posts/upload.html', context)
